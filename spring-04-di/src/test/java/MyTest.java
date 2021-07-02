903aea997461a8ba7b394d4e8680b9620918c405
zhuxiuwei <jlu_zxw@126.com>
2021-07-02 08:00:07 +0800

    spring-04-di

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -7,6 +7,13 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml") ;
         Student student = (Student)context.getBean("student");
         System.out.println(student);
-        System.out.println(student.getAddress());
     }
+
+    /**
+     * 输出：
+     * Student{name='top gun', address=Address{address='北京市海淀区'},
+     *      books=["红楼梦", "西游记"], hobbies=["音乐", "电影"],
+     *      card={身份证=身份照号码, 学生证=学生证号码}, games=["生化危机", "CS"],
+     *      info={籍贯="USA", 性别="二椅子"}, wife='null'}
+     */
 }
