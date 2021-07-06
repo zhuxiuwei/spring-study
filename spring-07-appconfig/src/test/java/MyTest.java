f58392d4f9bacf48434e4276f5238b251b5f8dec
zhuxiuwei <jlu_zxw@126.com>
2021-07-07 00:27:07 +0800

    '1'

--- spring-07-appconfig/src/test/java/MyTest.java
+++ spring-07-appconfig/src/test/java/MyTest.java
@@ -8,7 +8,8 @@ public class MyTest {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         User user = context.getBean("getUser", User.class);     //根据配置类MyConfig里方法名getUser表示bean id，获取bean
-        User user2 = context.getBean("user", User.class);   //能工作有2个前提：1). 配置类里加"@ComponentScan("com.xiuwei")" 2). User加Component注解。
+        User user2 = context.getBean("user", User.class);   //byType或byName获取。能工作有2个前提：1). 配置类里加"@ComponentScan("com.xiuwei")" 2). User加Component注解。
+//        User user3 = context.getBean("user222", User.class);   //不work
         System.out.println(user);
         System.out.println(user2);
         System.out.println(user == user2);  //false
