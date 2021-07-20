6e40435890dc6f6a79c2b041c7a75450c1698d51
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-07-20 21:24:37 +0800

    aop方式3:自定义类方式

--- spring-09-aop1/src/main/java/com/xiuwei/diy/DIYPointCut.java
+++ spring-09-aop1/src/main/java/com/xiuwei/diy/DIYPointCut.java
@@ -5,7 +5,7 @@ package com.xiuwei.diy;
  * 这个类 就是一个 切面(aspect)
  * 这个类 里的方法 就是 通知(advice)
  *
- * 没有Spring API接口方式 强大，因为Spring API接口
+ * 没有Spring API接口方式 强大，因为Spring API接口 可以用反射实现更多功能（获取method、args、target等）
  */
 public class DIYPointCut {
     public void before(){
