ac07a63618cbd33b5d6069ce0fc1b9a7e0907a7c
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-07-29 13:45:47 +0800

    spring-11-transaction

---
+++ spring-11-transaction/src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test1(){
+    }
+}
